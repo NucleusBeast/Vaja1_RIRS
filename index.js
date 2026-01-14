c5d41891d405c8c386530ca33b9865a1727887ce
NucleusBeast <36455332+NucleusBeast@users.noreply.github.com>
2026-01-14 10:57:32 +0100

    test docker build8

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 console.log('Happy developing ✨ Test!');
 //   store: MongoStore.create({ mongoUrl: mongoDB })
 // });
+// //Shranimo sejne spremenljivke v locals
+// //Tako lahko do njih dostopamo v vseh view-ih (glej layout.hbs)
